1483fd236969ea27f587f7931a2486a6710fecc5
Lucas Do Amaral <lucasdoamaralmartins@live.vu.edu.au>
2024-09-04 17:14:48 +1000

    user-creation-upd

--- student/models.py
+++ student/models.py
@@ -3,17 +3,19 @@ from django.contrib.auth.models import User
 
 
 class Student(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='static/profile_pic/Student/',null=False,blank=False)
-    address = models.CharField(max_length=40)
-    mobile = models.CharField(max_length=20,null=False)
-   
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, unique=True)
+    mobile = models.CharField(max_length=20, null=False)
+
     @property
     def get_name(self):
-        return self.user.first_name+" "+self.user.last_name
+        return self.user.first_name + " " + self.user.last_name
+
     @property
     def get_instance(self):
         return self
+
     def __str__(self):
-        return self.user.first_name
+        return self.user.username
+
         
--- teacher/models.py
+++ teacher/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Teacher(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='static/profile_pic/Teacher/',null=False,blank=False)
-    address = models.CharField(max_length=40)
-    mobile = models.CharField(max_length=20,null=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, unique=True)
+    mobile = models.CharField(max_length=20, null=False)
     status= models.BooleanField(default=False)
+
     @property
     def get_name(self):
-        return self.user.first_name+" "+self.user.last_name
+        return self.user.first_name + " " + self.user.last_name
+
     @property
     def get_instance(self):
         return self
+
     def __str__(self):
-        return self.user.first_name
+        return self.user.username
